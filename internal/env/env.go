a20cf97fb30f856047d242db760eb3a5b31427b9
jaronnie <jaron@jaronnie.com>
2022-12-07 22:45:54 +0800

    fix(httpsdk): fix get plugin env

---
+++ internal/env/env.go
@@ -0,0 +1,37 @@
+package env
+
+import (
+	"flag"
+
+	"github.com/spf13/viper"
+
+	"github.com/jaronnie/protoc-gen-go-httpsdk/utilx"
+)
+
+type PluginEnv struct {
+	GoVersion                         string   `validate:"required"`
+	GoModule                          string   `validate:"required"`
+	ScopeVersion                      string   `validate:"required"` // scopeVersion
+	ScopeVersions                     []string // scopeVersions used for clientSet
+	GatewayPrefix                     string   // microservice gateway prefix
+	IsWarpHttpResponse                bool     // is warped code, data, message
+	IsResourceExpansionCreateOrUpdate bool     // is to create or update resource expansion
+}
+
+func GetPluginEnv() (*PluginEnv, error) {
+	var c PluginEnv
+
+	if err := viper.Unmarshal(&c); err != nil {
+		return nil, err
+	}
+
+	if flag.CommandLine.Lookup("scopeVersion").Value.String() != "" {
+		c.ScopeVersion = flag.CommandLine.Lookup("scopeVersion").Value.String()
+	}
+
+	if err := utilx.ValidateStruct(c); err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
